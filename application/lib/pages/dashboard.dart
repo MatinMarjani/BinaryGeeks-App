1bc134b3f7da5a014cdae322551f03e9aaf4b9a7
amirzakuchaki <amirzakuchaki@gmail.com>
2021-04-16 23:03:31 +0430

    Add Empty dashboard template to main.dart

---
+++ application/lib/pages/dashboard.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+//import 'package:loginsignup/pages/widgets/myDrawer.dart';
+//import 'package:loginsignup/pages/widgets/myAppBar.dart';
+
+class DashBoard extends StatefulWidget {
+  @override
+  _DashBoardState createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        //child: MyAppBar(),
+      ),
+      body: Center(child: Text("داشبورد")),
+      //drawer: MyDrawer(),
+    );
+  }
+}
