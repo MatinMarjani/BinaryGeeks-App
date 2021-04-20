37161a69b0d0aa9777c0a9e9fed8762dae932b6b
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2021-04-20 13:02:36 +0430

    Update dashboard.dart
    dashboard now can use the new myPostCard widget.

--- application/lib/pages/dashboard.dart
+++ application/lib/pages/dashboard.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:flutter/services.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:application/pages/widgets/myDrawer.dart';
 import 'package:application/pages/widgets/myAppBar.dart';
-
+import 'package:application/pages/widgets/myPostCard.dart';
 
 class DashBoard extends StatefulWidget {
   @override
@@ -10,6 +14,11 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  void initState() {
+    super.initState();
+    log("Dashboard init");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +26,39 @@ class _DashBoardState extends State<DashBoard> {
         preferredSize: const Size.fromHeight(50),
         child: MyAppBar(),
       ),
-      body: Center(child: Text("داشبورد")),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: ListView(children: <Widget>[
+          Posts(),
+        ]),
+      ),
       drawer: MyDrawer(),
+    );
+  }
+
+  Container Posts() {
+    return Container(
+      child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          new PostCard("title", "author", "category", "price", "province",
+              "description"),
+          SizedBox(
+            height: 20,
+          ),
+          new PostCard("1", "author", "category", "price", "province",
+              "description"),
+          SizedBox(
+            height: 20,
+          ),
+          new PostCard("2", "author", "category", "price", "province",
+              "description"),
+          SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
     );
   }
 }
