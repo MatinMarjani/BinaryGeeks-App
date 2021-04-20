a54909310e67b869960e9841b17b7a5f680d75ea
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2021-04-20 14:41:30 +0430

    Add getPost() to dashboard.dart
    the getPost function calls the back to get the posts.
    the request is nost compelete.
    respons.data will be mapped to the class Post.
    its not connected to UI.

--- application/lib/pages/dashboard.dart
+++ application/lib/pages/dashboard.dart
@@ -4,9 +4,16 @@ import 'package:flutter/services.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:application/pages/widgets/myDrawer.dart';
 import 'package:application/pages/widgets/myAppBar.dart';
 import 'package:application/pages/widgets/myPostCard.dart';
+
+import 'package:application/util/app_url.dart';
+import 'package:application/util/Post.dart';
 
 class DashBoard extends StatefulWidget {
   @override
@@ -14,9 +21,12 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  List<Post> myPost = [];
+
   void initState() {
     super.initState();
     log("Dashboard init");
+    //getPosts();
   }
 
   @override
@@ -47,18 +57,72 @@ class _DashBoardState extends State<DashBoard> {
           SizedBox(
             height: 20,
           ),
-          new PostCard("1", "author", "category", "price", "province",
-              "description"),
+          new PostCard(
+              "1", "author", "category", "price", "province", "description"),
           SizedBox(
             height: 20,
           ),
-          new PostCard("2", "author", "category", "price", "province",
-              "description"),
+          new PostCard(
+              "2", "author", "category", "price", "province", "description"),
           SizedBox(
             height: 20,
           ),
         ],
       ),
     );
+  }
+
+  getPosts() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    var jsonResponse = null;
+    var response;
+
+    var token = sharedPreferences.getString("token");
+    var id = sharedPreferences.getInt("id").toString();
+    var url = Uri.parse("??????");
+
+    try {
+      response =
+          await http.get(url, headers: {'Authorization': 'Token $token'});
+      if (response.statusCode == 200) {
+        log('200');
+        print(response.body);
+        jsonResponse = json.decode(response.body);
+        if (jsonResponse != null) {
+          setState(() {
+            //_isLoading = false;
+          });
+        }
+      } else {
+        log('!200');
+        print(response.body);
+      }
+    } catch (e) {
+      print(e);
+    }
+
+    response = await http.get(url);
+    final extractedData = json.decode(response.body);
+    List posts = extractedData['post'];
+    for (var i in posts) {
+      myPost.add(Post(
+        i["id"],
+        i["title"],
+        i["author"],
+        i["publisher"],
+        i["price"],
+        i["province"],
+        i["city"],
+        i["zone"],
+        i["status"],
+        i["description"],
+        i["is_active"],
+        i["image"],
+        //url
+        i["categories"],
+        i["created_at"],
+      ));
+    }
   }
 }
