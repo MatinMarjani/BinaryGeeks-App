2c248dfc6480b640610dcc9444f58a5baa849c56
Amin <amirzakuchaki@gmail.com>
2021-04-21 16:56:52 +0430

    Update myPostCard.dart
    better card view for Post Cards

--- application/lib/pages/widgets/myPostCard.dart
+++ application/lib/pages/widgets/myPostCard.dart
@@ -9,8 +9,7 @@ class PostCard extends StatelessWidget {
   String province;
   String description;
 
-  String url =
-      "https://www.kindpng.com/picc/m/73-738543_collection-of-free-books-drawing-easy-download-on.png";
+  String url = "https://wallpaperaccess.com/full/832966.jpg";
 
   PostCard(
     this.title,
@@ -25,35 +24,81 @@ class PostCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       borderOnForeground: true,
-      elevation: 15,
+      elevation: 5,
       margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
       color: Colors.white70,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30.0),
+        borderRadius: BorderRadius.circular(10.0),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(5),
+        padding: const EdgeInsets.all(2),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Container(
-              child:  Image.network(
-                url,
-                width: 150,
-                height: 150,
-              ),
-            ),
-            SizedBox(width: 20),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                Text(title,
-                style: ,),
-                Text(author),
-                Text(category),
-                Text(price),
-                Text(province),
-                Text(description),
+                Container(
+                  width: 100,
+                  height: 120,
+                  // color: Colors.blue,
+                  child: Image.network(
+                    url,
+                    width: 100,
+                    height: 120,
+                  ),
+                ),
               ],
             ),
+            SizedBox(width: 15),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12.0, 2.0, 2.0, 2.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12.0, 2.0, 2.0, 2.0),
+                    child: Text(
+                      author,
+                      style: TextStyle(fontSize: 15.0),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12.0, 2.0, 2.0, 2.0),
+                    child: Text(
+                      category,
+                      style: TextStyle(fontSize: 13.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(12.0, 2.0, 2.0, 2.0),
+                  child: Text(
+                    price,
+                    style: TextStyle(fontSize: 15.0, color: Colors.green),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(12.0, 2.0, 2.0, 2.0),
+                  child: Text(
+                    province,
+                    style: TextStyle(fontSize: 14.0),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
