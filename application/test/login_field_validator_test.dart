6683c784f1ba6793fdafda15fc1b390f632a318f
Amin <amirzakuchaki@gmail.com>
2021-04-21 20:33:24 +0430

    Create login_field_validator_test.dart

---
+++ application/test/login_field_validator_test.dart
@@ -0,0 +1,24 @@
+import 'package:test/test.dart';
+import 'package:application/pages/login.dart';
+
+void main () {
+   test("Empty email return error",(){
+     var result = EmailFieldValidator.validate('');
+     expect(result, 'الزامی است');
+   });
+
+   test("non-Empty email return null",(){
+     var result = EmailFieldValidator.validate('email');
+     expect(result, null);
+   });
+
+   test("Empty password return error",(){
+     var result = PasswordFieldValidator.validate('');
+     expect(result, 'الزامی است');
+   });
+
+   test("non-Empty password return error",(){
+     var result = PasswordFieldValidator.validate('password');
+     expect(result, null);
+   });
+}
