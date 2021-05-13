e72ec6f446e0862a56bc71714f6dc0e376314d81
Matin <matinmarjani@gmail.com>
2021-05-13 15:34:54 +0430

    Add pull down to refresh and pull up to load more (Pagination) to dashboard.dart

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'package:flutter/services.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'package:application/pages/widgets/myDrawer.dart';
 import 'package:application/pages/widgets/myAppBar.dart';
@@ -14,10 +14,11 @@ import 'package:application/pages/widgets/myPostCard.dart';
 
 import 'package:application/util/app_url.dart';
 import 'package:application/util/Post.dart';
+import 'package:application/util/Utilities.dart';
 
 class DashBoard extends StatefulWidget {
-  final Color mainColor = Colors.blue[800];
-  final String myFont = 'myFont';
+  final Color mainColor = Utilities().mainColor;
+  final String myFont = Utilities().myFont;
 
 
   @override
@@ -27,16 +28,44 @@ class DashBoard extends StatefulWidget {
 class _DashBoardState extends State<DashBoard> {
   List<Post> myPost = [];
   bool _isLoading = false;
+  int page;
+
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
 
   void initState() {
     super.initState();
     log("Dashboard init");
     setState(() {
+      _isLoading = true;
+      page = 1;
       MyAppBar.appBarTitle =
-          Text("BookTrader", style: TextStyle(color: Colors.white, fontFamily: 'myfont'));
+          Text("داشبورد", style: TextStyle(color: Colors.white, fontFamily: 'myfont'));
       MyAppBar.actionIcon = Icon(Icons.search, color: Colors.white);
+      myPost.clear();
     });
-    getPosts();
+    getPosts(page.toString());
+  }
+
+  void _onRefresh() async{
+    myPost.clear();
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    for ( int i = 1; i <= page; i++) {
+      getPosts(page.toString());
+    }
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async{
+    // monitor network fetch
+    page++;
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    getPosts(page.toString());
+    if(mounted)
+      setState(() {});
+    _refreshController.loadComplete();
   }
 
   @override
@@ -46,25 +75,54 @@ class _DashBoardState extends State<DashBoard> {
         preferredSize: const Size.fromHeight(50),
         child: MyAppBar(),
       ),
-      body: Container(
-        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      body: SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: true,
+        header: WaterDropHeader(),
+        footer: CustomFooter(
+          builder: (BuildContext context,LoadStatus mode){
+            Widget body ;
+            if(mode==LoadStatus.idle){
+              body =  Text("pull up load");
+            }
+            else if(mode==LoadStatus.loading){
+              body =  CupertinoActivityIndicator();
+            }
+            else if(mode == LoadStatus.failed){
+              body = Text("Load Failed!Click retry!");
+            }
+            else if(mode == LoadStatus.canLoading){
+              body = Text("release to load more");
+            }
+            else{
+              body = Text("No more Data");
+            }
+            return Container(
+              height: 55.0,
+              child: Center(child:body),
+            );
+          },
+        ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
         child: _isLoading
-            ? Center(
-            child: CircularProgressIndicator(
-              valueColor: new AlwaysStoppedAnimation<Color>(widget.mainColor),
-            ))
-            : ListView(children: <Widget>[
-          Posts(),
-        ]),
+              ? Center(
+              child: CircularProgressIndicator(
+                valueColor: new AlwaysStoppedAnimation<Color>(widget.mainColor),
+              ))
+              : ListView(children: <Widget>[
+            posts(),
+          ]),
+
       ),
       drawer: MyDrawer(),
     );
   }
 
-  Widget Posts() {
+  Widget posts() {
     if (myPost.length == 0) return Text("nothing");
-    List<Widget> list = new List<Widget>();
+    List<Widget> list = [];
     for (var i = 0; i < myPost.length; i++) {
       if (myPost[i].title == null) myPost[i].title = " ";
       if (myPost[i].author == null) myPost[i].author = " ";
@@ -78,18 +136,12 @@ class _DashBoardState extends State<DashBoard> {
     return Column(children: list);
   }
 
-  getPosts() async {
-    setState(() {
-      _isLoading = true;
-      myPost.clear();
-    });
+  getPosts(String p) async {
 
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     var jsonResponse;
     var response;
-
-    var url = Uri.parse(AppUrl.Get_My_Posts);
-    log(AppUrl.Get_My_Posts);
+    var url = Uri.parse(AppUrl.Get_My_Posts + "?page=" + p);
     var token = sharedPreferences.getString("token");
 
     try {
@@ -126,12 +178,16 @@ class _DashBoardState extends State<DashBoard> {
         }
       } else {
         log('!200');
-        print(response.body);
+        jsonResponse = json.decode(utf8.decode(response.bodyBytes));
+        setState(() {
+          if(jsonResponse["detail"] == "Invalid page.")
+            page--;
+        });
       }
     } catch (e) {
       log("error");
-      print(e);
     }
+
     setState(() {
       _isLoading = false;
     });
